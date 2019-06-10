555216d2ec4bcb874e07fe5c3ffc89a62b6cb1c0
William Flores <williamflores@WFLORES-MBP-DAL.local>
2019-06-10 13:37:32 -0500

    add wireframe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EventCardComponent } from './components/event-card/event-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventCardComponent
   ],
   imports: [
     BrowserModule,
