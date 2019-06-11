60df8cc4f7fd39c318e19b8be74c71dcfb9e066f
Watson <Graham.Watson@parivedasolutions.com>
2019-06-11 15:54:00 -0500

    added detail modal

--- src/app/components/createbutton/createbutton.component.ts
+++ src/app/components/createbutton/createbutton.component.ts
@@ -13,21 +13,7 @@ export class CreatebuttonComponent {
   constructor(private modalService: NgbModal) {}
 
   open(content) {
-    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
-      this.closeResult = `Closed with: ${result}`;
-    }, (reason) => {
-      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-    });
-  }
-
-  private getDismissReason(reason: any): string {
-    if (reason === ModalDismissReasons.ESC) {
-      return 'by pressing ESC';
-    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-      return 'by clicking on a backdrop';
-    } else {
-      return  `with: ${reason}`;
-    }
+    this.modalService.open(content);
   }
 
 }
--- src/app/components/event-card/event-card.component.ts
+++ src/app/components/event-card/event-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-event-card',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventCardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private modalService: NgbModal) { }
 
   ngOnInit() {
+  }
+
+  showModal(content, event:any) {
+    let classList = event.target.classList;
+    if(classList.contains('btn')) {
+      return;
+    }
+    this.modalService.open(content);
   }
 
 }
