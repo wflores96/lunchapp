fa152dfba005db69db68b167d20fa7ed9d9f1307
William Flores <williamflores@WFLORES-MBP-DAL.local>
2019-06-13 11:30:03 -0500

    added going and undo functionality

--- src/app/components/event-card/event-card.component.ts
+++ src/app/components/event-card/event-card.component.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit, Input } from '@angular/core';
-import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 
+
+interface IData {
+  location: string,
+  time: Date,
+  host: string,
+  going: [string],
+  maybe: [string]
+}
 @Component({
   selector: 'app-event-card',
   templateUrl: './event-card.component.html',
@@ -8,19 +16,49 @@ import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 })
 export class EventCardComponent implements OnInit {
 
-  @Input() data:any;
+  @Input() data: IData;
+  userGoing = false;
 
   constructor(private modalService: NgbModal) { }
 
   ngOnInit() {
+    var me = localStorage.getItem('name');
+    if(this.data.going.indexOf(me) != -1 || this.data.maybe.indexOf(me) != -1) {
+      this.userGoing = true;  
+    }
   }
 
-  showModal(content, event:any) {
+  showModal(content, event: any) {
     let classList = event.target.classList;
-    if(classList.contains('btn') || classList.contains('badge')) {
+    if (classList.contains('btn') || classList.contains('badge')) {
       return;
     }
     this.modalService.open(content);
+  }
+
+  clickMaybe() {
+    this.userGoing = true;
+    this.data.maybe.push(localStorage.getItem('name'));
+  }
+
+  clickGoing() {
+    this.userGoing = true;
+    this.data.going.push(localStorage.getItem('name'));
+  }
+
+  clickUndo() {
+    var me = localStorage.getItem('name');
+
+    this.userGoing = false;
+    var index = this.data.going.indexOf(me);
+    if(index != -1) {
+      this.data.going.splice(index, 1);
+    }
+
+    index = this.data.maybe.indexOf(me);
+    if(index != -1) {
+      this.data.maybe.splice(index, 1);
+    }
   }
 
 }
